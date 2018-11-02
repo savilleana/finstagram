13383bac021de792c16d4f4a5b5ff6441fb6ab81
Ana Saville <savilleana@gmail.com>
2018-11-02 00:31:08 +0000

    heroku update

--- app/actions.rb
+++ app/actions.rb
@@ -8,10 +8,7 @@ get '/' do
   erb(:index)
 end
 
-get '/signup' do     # if a user navigates to the path "/signup",
-  @user = User.new   # setup empty @user object
-  erb(:signup)       # render "app/views/signup.erb"
-end
+
 
 post '/signup' do
   email      = params[:email]
@@ -22,7 +19,7 @@ post '/signup' do
   @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
 
   if @user.save
-    redirect to('/login')
+    "User #{username} saved!"
   else
     erb(:signup)
   end
@@ -69,4 +66,26 @@ end
 get '/finstagram_posts/:id' do
   @finstagram_post = FinstagramPost.find(params[:id])   # find the finstagram post with the ID from the URL
   erb(:"finstagram_posts/show")               # render app/views/finstagram_posts/show.erb
+end
+post '/comments' do
+  # point values from params to variables
+  text = params[:text]
+  finstagram_post_id = params[:finstagram_post_id]
+
+  # instantiate a comment with those values & assign the comment to the `current_user`
+  comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+
+  # save the comment
+  comment.save
+
+  # `redirect` back to wherever we came from
+  redirect(back)
+end
+post '/likes' do
+  finstagram_post_id = params[:finstagram_post_id]
+
+  like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+  like.save
+
+  redirect(back)
 end
--- config/database.rb
+++ config/database.rb
@@ -1,8 +1,19 @@
-configure do
-  # Log queries to STDOUT in development
-  if Sinatra::Application.development?
-    ActiveRecord::Base.logger = Logger.new(STDOUT)
-  end
+ if Sinatra::Application.development?
+   set :database, {
+     adapter: "sqlite3",
+     database: "db/db.sqlite3"
+   }
+ else
+   db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+   set :database, {
+     adapter: "postgresql",
+     host: db.host,
+     username: db.user,
+     password: db.password,
+     database: db.path[1..-1],
+     encoding: 'utf8'
+   }
+ end
 
   set :database, {
     adapter: "sqlite3",
