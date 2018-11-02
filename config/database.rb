47a4a0ec915338ab2af2b9ca919d6289655f8b63
Ana Saville <savilleana@gmail.com>
2018-11-02 00:40:27 +0000

    databaserb fix

--- config/database.rb
+++ config/database.rb
@@ -15,11 +15,6 @@
    }
  end
 
-  set :database, {
-    adapter: "sqlite3",
-    database: "db/db.sqlite3"
-  }
-
   # Load all models from app/models, using autoload instead of require
   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
   Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
